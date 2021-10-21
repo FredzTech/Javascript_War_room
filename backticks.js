f27c3bdd318ac81b19f47a3c388c2cf10382d02a
Alfred <alfredgithinji87@gmail.com>
2021-10-21 20:46:41 +0300

    .

--- backticks.js
+++ backticks.js
@@ -1,6 +1,9 @@
+//Backticks simply make your code lots of cleaner and readable
+//Also minimizes the needs of escaping like in the sentence created
 const f_name = "Alfred";
 const m_name = "Githinji";
 const l_name = "Gichia";
+const sentence = `Tunang'ag'ana na kukazana kimaisha.`
 
 const full_name = f_name+" "+ m_name+" " + l_name;
 
@@ -8,3 +11,4 @@ const full_name2 = `${f_name} ${m_name} ${l_name}`
 
 console.log(full_name);
 console.log(full_name2);
+console.log(sentence);
